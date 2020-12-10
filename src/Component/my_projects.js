0d9af3233d31dbacf6b3ef40527f79f6c594508a
vienpham2019 <vienpham2019@gmail.com>
2020-12-10 10:59:35 -0600

    save

--- src/Component/my_projects.js
+++ src/Component/my_projects.js
@@ -1,68 +1,104 @@
 const my_projects = [
-    {
-        title: "vShop.", 
-        description: "An activewear/accessories e-commerce site for men and women.",
-        details: ["Built client-side with Angular/NgRx.", "Utilized NodeJS with API endpoints for Fetch requests and MongoDB for data persistence." , "Styled frontend with Bootstrap and custom CSS." , "Deployed application on Heroku."], 
-        resume_description: "An activewear/accessories e-commerce site for men and women.",
-        tools: "Angular | NgRx | HTML | CSS | Bootstrap | NodeJS | Express | MongoDB",
-        github: "https://github.com/vienpham2019/vShop.",
-        demo: "https://v--shop.herokuapp.com/",
-        video: false,
-        image: "project_img/v-shop.png"
-    },
-    {
-        title: "Maze Generator", 
-        description: "Maze generation algorithms are automated methods for the creation of mazes.",
-        details: ["Utilized React and designed with custom Css for front-end.", "Deploy application to Firebase."], 
-        tools: "ReactJS | Redux | HTML | CSS | Bootstrap",
-        github: "https://github.com/vienpham2019/Maze_generator",
-        demo: "https://mazegenerator-a4b1a.firebaseapp.com/",
-        video: false,
-        image: "project_img/maze_generator.png"
-    },
-    {
-        title: "Vintage Village",
-        description: "Vintage Village will exceed your expectations in every way. A serene setting where you can enjoy nature and relish luxury and comfort. And where there’s always something new to enjoy. A place where we’ll indulge your every wish and help you rediscover the simple things in life.",
-        resume_description: 'A serene setting where you can enjoy nature and relish luxury and comfort.', 
-        details: ["Utilized React/Redux for front-end." , "Deploy application on Heroku." , "Designed the frontend with Bootstrap and custom CSS.", "Used NodeJS for back-end and MongoDB for database."],
-        tools: "NodeJS | Express | ReactJS | Redux | HTML | CSS | Bootstrap | MongoDB",
-        github: "https://github.com/vienpham2019/beachResort",
-        demo: "https://beackresortsdemo.herokuapp.com/",
-        video: false,
-        image: "project_img/vintage_village.png"
-    },
-    {
-        title: "Movies",
-        description: "Create a favorite movie list, write reviews, share movie collections and chat with other movie fans.", 
-        resume_description: 'Create a favorite movie list, write reviews, share movie collections and chat with other movie fans.', 
-        details: ["Utilized React/Redux for frontend and Socket.io to add/chat with friends between client-side.", "Used Ruby-on-Rails to design the backend with the MVC pattern and API for responding to fetch requests.", "Designed the frontend with Bootstrap and custom CSS."], 
-        tools: "Ruby on rails | ReactJS | Redux | Socket.io | HTML | CSS | Bootstrap",
-        github: "https://github.com/vienpham2019/movie_app_module_5_project",
-        demo: "https://www.youtube.com/watch?v=GHcF0JR4XH4&t=1s",
-        video: true, 
-        image: "project_img/movies.jpg"
-    }, 
-    {
-        title: "League Tracker", 
-        description: "League Tracker was designed to help users to track he/she or other League of Legends match history, ranked tier and view hundred champion’s info in game.",
-        resume_description: 'A great way to track your League of Legends account.', 
-        details: ["Built the frontend with React, and Ruby on Rails for backend with JSON token for authentication.", "Fetch League of Legends API from backend." , "Styled frontend with Bootstrap, CSS and Semantic UI React."], 
-        tools: "Ruby on Rails | ReactJS | HTML | CSS | Bootstrap", 
-        github: "https://github.com/vienpham2019/league_tracker",
-        demo: "https://www.youtube.com/watch?v=B14-_Xz_IBc&t=3s",
-        video: true, 
-        image: "project_img/league_of_legends_tracker.png"
-    }, 
-    {
-        title: "Black Jack", 
-        description: "Blackjack is a casino banked game, meaning that players compete against the house rather than each other. The objective is to get a hand total of closer to 21 than the dealer without going over 21 (busting).", 
-        details: ["Created frontend and backend with Ruby on Rails application.", "Used CSS to design the frontend."], 
-        tools: "Ruby on Rails | HTML | CSS",
-        github: "https://github.com/vienpham2019/mod2-blackjack-project",
-        demo: "https://www.youtube.com/watch?v=aLIpii6evFc&feature=youtu.be",
-        video: true, 
-        image: "project_img/blackjack.jpg"
-    }
-]
+  {
+    title: "vShop.",
+    description: "An activewear/accessories e-commerce site for men and women.",
+    details: [
+      "Built client-side with Angular/NgRx.",
+      "Utilized NodeJS with API endpoints for Fetch requests and MongoDB for data persistence.",
+      "Styled frontend with Bootstrap and custom CSS.",
+      "Deployed application on Heroku.",
+    ],
+    resume_description:
+      "An activewear/accessories e-commerce site for men and women.",
+    tools:
+      "Angular | NgRx | HTML | CSS | Bootstrap | NodeJS | Express | MongoDB",
+    github: "https://github.com/vienpham2019/vShop.",
+    demo: "https://v--shop.herokuapp.com/",
+    video: false,
+    image: "project_img/v-shop.png",
+  },
+  {
+    title: "The Movies",
+    description:
+      "Create a favorite movie list, write reviews and share movie collections.",
+    resume_description:
+      "Create a favorite movie list, write reviews and share movie collections.",
+    details: [
+      "Utilized React/Redux for frontend.",
+      "Used Ruby-on-Rails to design the backend with the MVC pattern and API for responding to fetch requests.",
+      "Designed the frontend with Bootstrap and custom CSS.",
+    ],
+    tools:
+      "Ruby on rails | ReactJS | Redux | Socket.io | HTML | CSS | Bootstrap",
+    github: "https://github.com/vienpham2019/The_Movies",
+    demo: "https://the-movies-f6ace.web.app/",
+    video: false,
+    image: "project_img/The_Movies.png",
+  },
+  {
+    title: "Maze Generator",
+    description:
+      "Maze generation algorithms are automated methods for the creation of mazes.",
+    details: [
+      "Utilized React and designed with custom Css for front-end.",
+      "Deploy application to Firebase.",
+    ],
+    tools: "ReactJS | Redux | HTML | CSS | Bootstrap",
+    github: "https://github.com/vienpham2019/Maze_generator",
+    demo: "https://mazegenerator-a4b1a.firebaseapp.com/",
+    video: false,
+    image: "project_img/maze_generator.png",
+  },
+  {
+    title: "Vintage Village",
+    description:
+      "Vintage Village will exceed your expectations in every way. A serene setting where you can enjoy nature and relish luxury and comfort. And where there’s always something new to enjoy. A place where we’ll indulge your every wish and help you rediscover the simple things in life.",
+    resume_description:
+      "A serene setting where you can enjoy nature and relish luxury and comfort.",
+    details: [
+      "Utilized React/Redux for front-end.",
+      "Deploy application on Heroku.",
+      "Designed the frontend with Bootstrap and custom CSS.",
+      "Used NodeJS for back-end and MongoDB for database.",
+    ],
+    tools:
+      "NodeJS | Express | ReactJS | Redux | HTML | CSS | Bootstrap | MongoDB",
+    github: "https://github.com/vienpham2019/beachResort",
+    demo: "https://beackresortsdemo.herokuapp.com/",
+    video: false,
+    image: "project_img/vintage_village.png",
+  },
 
-export default my_projects
+  {
+    title: "League Tracker",
+    description:
+      "League Tracker was designed to help users to track he/she or other League of Legends match history, ranked tier and view hundred champion’s info in game.",
+    resume_description: "A great way to track your League of Legends account.",
+    details: [
+      "Built the frontend with React, and Ruby on Rails for backend with JSON token for authentication.",
+      "Fetch League of Legends API from backend.",
+      "Styled frontend with Bootstrap, CSS and Semantic UI React.",
+    ],
+    tools: "Ruby on Rails | ReactJS | HTML | CSS | Bootstrap",
+    github: "https://github.com/vienpham2019/league_tracker",
+    demo: "https://www.youtube.com/watch?v=B14-_Xz_IBc&t=3s",
+    video: true,
+    image: "project_img/league_of_legends_tracker.png",
+  },
+  {
+    title: "Black Jack",
+    description:
+      "Blackjack is a casino banked game, meaning that players compete against the house rather than each other. The objective is to get a hand total of closer to 21 than the dealer without going over 21 (busting).",
+    details: [
+      "Created frontend and backend with Ruby on Rails application.",
+      "Used CSS to design the frontend.",
+    ],
+    tools: "Ruby on Rails | HTML | CSS",
+    github: "https://github.com/vienpham2019/mod2-blackjack-project",
+    demo: "https://www.youtube.com/watch?v=aLIpii6evFc&feature=youtu.be",
+    video: true,
+    image: "project_img/blackjack.jpg",
+  },
+];
+
+export default my_projects;
